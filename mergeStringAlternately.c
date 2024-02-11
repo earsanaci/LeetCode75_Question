6df4df63db7024d3b7cb7f05ddd8ac5efe7ccf48
Emre ARSANACI <earsanaci@gmail.com>
2024-02-11 20:56:41 +0300

    Added the first solutions.

---
+++ mergeStringAlternately.c
@@ -0,0 +1,27 @@
+char * mergeAlternately(char * word1, char * word2)
+{
+    int len1 = strlen(word1);
+    int len2 = strlen(word2);
+
+    int totalLength = len1 + len2;
+
+    char* result = malloc((totalLength + 1) * sizeof(char));
+
+    if (result == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    int i, k = 0;
+    for (i = 0; i < len1 || i < len2; i++) {
+        if (i < len1) {
+            result[k++] = word1[i];
+        }
+        if (i < len2) {
+            result[k++] = word2[i];
+        }
+    }
+    result[k] = '\0';
+
+    return result;
+}
